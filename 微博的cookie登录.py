8c7109bc2af1fb7f9907ce1be0fe0167df994c78
Caicai702 <110366893+Caicai702@users.noreply.github.com>
2022-08-01 11:49:28 +0800

    Update 微博的cookie登录.py

--- 微博的cookie登录.py
+++ 微博的cookie登录.py
@@ -12,7 +12,7 @@ headers={
 #'accept-encoding':' gzip, deflate, br',
 'accept-language':' zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
 'cache-control':' max-age=0',
-'cookie':'login_sid_t=6f87e8775d28ce64e89437256655128a; cross_origin_proto=SSL; _s_tentry=passport.weibo.com; Apache=5468084505293.436.1659317578551; SINAGLOBAL=5468084505293.436.1659317578551; ULV=1659317578555:1:1:1:5468084505293.436.1659317578551:; wb_view_log=1536*8641.25; WBtopGlobal_register_version=2022080109; SUB=_2A25P410mDeRhGeNH41UW-CjNyzuIHXVsmcnurDV8PUNbmtAKLWLTkW9NSnjxpHPU5r3Qgq8pW2IB3uJAOPrB7bKX; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W56yuUOKYyxk0JRQRe8iH2Q5JpX5o275NHD95Qf1KnNS0nceK5NWs4Dqcji-cQ41KM7eLvA; ALF=1659922422; SSOLoginState=1659317622; wvr=6; wb_view_log_5987786107=1536*8641.25; PC_TOKEN=d59b33c58e; webim_unReadCount=%7B%22time%22%3A1659320082060%2C%22dm_pub_total%22%3A106%2C%22chat_group_client%22%3A0%2C%22chat_group_notice%22%3A1%2C%22allcountNum%22%3A253%2C%22msgbox%22%3A0%7D',
+'cookie':'这里放自己的cookie',
 'sec-ch-ua':' " Not;A Brand";v="99", "Microsoft Edge";v="103", "Chromium";v="103"',
 'sec-ch-ua-mobile':' ?0',
 'sec-ch-ua-platform':' "Windows"',
